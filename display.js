34c26c32c840ee266ad8090b42ed78fb5bc6f0c8
Ylia M <ysmorid@users.noreply.github.com>
2017-09-27 17:55:35 -0500

    F1S4 - Add capability for clicking on flashcard multiple times to toggle between original word and its translation

--- display.js
+++ display.js
@@ -5,3 +5,14 @@ displayFlashcard = () => {
 displayTranslation = () => {
   return document.getElementsByClassName('flashcard')[0].innerHTML = flashcard.translate();
 };
+
+let counter = 0;
+toggleFlashcardWordAndTranslation = () => {
+  counter += 1;
+
+  if(counter %2 ===0) {
+    return displayFlashcard();
+  } else {
+    return displayTranslation();
+  }
+};
